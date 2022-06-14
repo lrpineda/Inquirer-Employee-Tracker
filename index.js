f610579077ce2d13dd46ff5b46c74d6ad8a1bd1d
lrpineda <luicks212@gmail.com>
2022-06-14 01:36:16 -0600

    Adding queries

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const db = require('./db/connection');
-const cTable = require('console.table');
+const { viewAllDepartments, 
+        viewAllRoles, 
+        viewAllEmployees } = require('./lib/queries');
 
-const sql = `SELECT e.id AS ID, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager
-               FROM employee e
-               LEFT JOIN role ON e.role_id = role.id
-               LEFT JOIN department ON role.department_id = department.id
-               LEFT JOIN employee m ON m.id = e.manager_id`;
-
-db.query(sql, (err, rows) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.table(rows);
-    }
-});
+viewAllDepartments();
+viewAllRoles();
+viewAllEmployees();
