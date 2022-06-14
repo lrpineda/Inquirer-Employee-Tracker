583c36f9570899cc6d00fb40e95f2dae93b73ab3
lrpineda <luicks212@gmail.com>
2022-06-14 00:20:16 -0600

    Starting index file to show employee info

--- db/seeds.sql
+++ db/seeds.sql
@@ -56,8 +56,3 @@ VALUES
 ('Steven', 'Grant', 1, 3),
 ('Marc', 'Spector', 3, 3);
 
-SELECT e.id AS ID, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager
-FROM employee e
-LEFT JOIN role ON e.role_id = role.id
-LEFT JOIN department ON role.department_id = department.id
-LEFT JOIN employee m ON m.id = e.manager_id;
